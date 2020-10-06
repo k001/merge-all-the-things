a3dd0dd380ce28a5c12dc94172bab1fb9ca2c18c
Ivan Zenteno <243380+k001@users.noreply.github.com>
2020-10-06 13:14:32 -0500

    Update to use PEP8
    To follow python conventions with pep8

--- background.py
+++ background.py
@@ -2,9 +2,9 @@ import cv2
 # THIS IS MY WEBCAM
 cap = cv2.VideoCapture(0)
 
-#Repeat loop for when the webcam is open
+# Repeat loop for when the webcam is open
 while cap.isOpened():
-    ret, back = cap.read() #HERE IS AM SIMPLY READING FROM MY WEBCAM
+    ret, back = cap.read()  # HERE IS AM SIMPLY READING FROM MY WEBCAM
     if ret:
         # back is what the camera is reading
         cv2.imshow("image", back)
@@ -13,6 +13,10 @@ while cap.isOpened():
             cv2.imwrite('image.jpg', back)
             break
 
-#Releasing the lock
-cap.release()
-cv2.destroyAllWindows()
+
+def main():
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
